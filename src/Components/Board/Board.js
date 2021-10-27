a319c41ab4415b196ad98dc270dff1d1e6c2fc19
cstegeman <c.stegeman@gmail.com>
2021-10-27 16:15:52 +0200

    Play a file using the web audio api

---
+++ src/Components/Board/Board.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("./sounds.json")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result,
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error,
+          });
+        }
+      );
+  }
+
+  render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>
+              <button type="button" onClick={() => this.play(item.location)}>
+                {item.name}
+              </button>
+            </li>
+          ))}
+        </ul>
+      );
+    }
+  }
+
+  play(location) {
+    const context = new AudioContext();
+    fetch("/audio/" + location)
+      .then((response) => response.arrayBuffer())
+      .then((arrayBuffer) => context.decodeAudioData(arrayBuffer))
+      .then((audioBuffer) => {
+        const source = context.createBufferSource();
+        source.buffer = audioBuffer;
+        source.connect(context.destination);
+        source.start();
+      });
+  }
+}
+
+export default Board;
