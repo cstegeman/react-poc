9c98262aa868e03bf2a4b8bfdaec689baea8bf50
cstegeman <c.stegeman@gmail.com>
2021-11-08 21:17:09 +0100

    Component placeholders

---
+++ src/components/Board.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Board = () => {
+  return (
+    <>
+      <Header />
+      <main>content</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Board;
