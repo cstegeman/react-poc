09e280c15a0237d141c39f55ee705d906b336486
cstegeman <c.stegeman@gmail.com>
2021-11-14 14:12:42 +0100

    Improve json generator script

--- tools/json-generator.js
+++ tools/json-generator.js
@@ -1,40 +1,65 @@
-import fs from 'fs-extra'
+import fs from "fs-extra";
 import NodeID3 from "node-id3";
 
-const OUTPUT = './public';
-const INPUT = './public/audio';
+const OUTPUT = "./public";
+const INPUT = "./public/audio";
 
 const getAllFiles = function (dirPath) {
-    const files = fs.readdirSync(dirPath)
+  const files = fs.readdirSync(dirPath);
 
-    return files.map(function(file) {
-        if (fs.statSync(dirPath + "/" + file).isDirectory()) {
-            return ({
-                title: file.replace('-', ' '),
-                category: file,
-                sounds: getAllFiles(dirPath + "/" + file)
-            })
-        }
-        return ({
-          title: getFileTitle(`${dirPath}/${file}`),
-          name: file,
-        });
-    })
-}
+  return files.map(function (file) {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      return {
+        title: getFileCategory(`${dirPath}/${file}`),
+        category: file,
+        sounds: getAllFiles(dirPath + "/" + file),
+      };
+    }
+    return {
+      title: getFileTitle(`${dirPath}/${file}`),
+      name: file,
+    };
+  });
+};
 
 /**
  * @param {string} file
  * @returns {string}
  */
 function getFileTitle(file) {
-    const tags = NodeID3.read(file)
-    NodeID3.read(file, function(err, tags) {})
+  const tags = NodeID3.read(file);
+  NodeID3.read(file, function (err, tags) {});
 
-    return tags.title
+  return tags.title;
 }
 
-const result = getAllFiles(INPUT)
+/**
+ * @param {string} dirPath
+ * @returns {string}
+ */
+function getFileCategory(dirPath) {
+  const files = fs.readdirSync(dirPath);
+  let i = 0;
+  let found = false;
+  let category = "";
 
-fs.writeFileSync(OUTPUT + '/sounds.json', JSON.stringify(result, null, 2));
+  while (i < files.length && !found) {
+    const file = files[i];
+    if (!fs.statSync(dirPath + "/" + file).isDirectory()) {
+      const tags = NodeID3.read(dirPath + "/" + file);
+      NodeID3.read(file, function (err, tags) {});
 
-console.log('All done!');
+      category = tags.album;
+      found = true;
+    }
+    i++;
+  }
+
+  return category;
+}
+
+const result = getAllFiles(INPUT);
+
+fs.writeFileSync(OUTPUT + "/sounds.json", JSON.stringify(result, null, 2));
+
+console.log("All done!");
