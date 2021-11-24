9918c098c7f114569862fff7b4dea3a52f2510cd
cstegeman <c.stegeman@gmail.com>
2021-11-24 20:35:58 +0100

    Button styling with material ui components

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,28 +1,35 @@
 import { play } from '../utils/audio';
+import { Box, Button, Divider, Grid, Typography } from '@mui/material';
 
 const Board = ({ files, reversePlayback, playSpeed, enableDelay }) => {
   return (
     <>
       {files.map((file, index) => {
         return (
-          <section key={index}>
-            <h2>{file.title}</h2>
-            <hr />
-            <div className="flex flex--wrap gap--3">
-              {file.sounds.map((sound, i) => {
-                return (
-                  <button
-                    key={i}
-                    onClick={() =>
-                      play(file.category, sound.name, playSpeed, reversePlayback, enableDelay)
-                    }
-                  >
-                    {sound.title}
-                  </button>
-                );
-              })}
-            </div>
-          </section>
+          <Box key={index} sx={{ mt: 3 }}>
+            <Typography variant="h4" component="h2">
+              {file.title}
+            </Typography>
+            <Divider variant="middle" />
+            <Box sx={{ mt: 2 }}>
+              <Grid container spacing={1}>
+                {file.sounds.map((sound, i) => {
+                  return (
+                    <Grid item xs="auto" key={i}>
+                      <Button
+                        variant="contained"
+                        onClick={() =>
+                          play(file.category, sound.name, playSpeed, reversePlayback, enableDelay)
+                        }
+                      >
+                        {sound.title}
+                      </Button>
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            </Box>
+          </Box>
         );
       })}
     </>
